35654cca1c561f155ead5d12b0a9dfc0eb490e88
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-02-09 16:27:50 +0100

    simple server with json "ok": true

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import express from 'express';
+import setupMiddware from './middleware';
+import { restRouter } from './api';
+import { connect } from './db';
+import { signin, protect } from './api/modules/auth';
 // Declare an app from express
-const app = null
+const app = express();
 
-export default app
+setupMiddware(app);
+connect();
+// setup basic routing for index route
+
+app.use('/signin', signin);
+
+// catch all
+app.all('*', (req, res) => {
+  res.json({ ok: true });
+});
+
+export default app;
