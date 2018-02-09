de5fcffe44f3dc67010803b44437e6f70b543058
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-02-09 18:00:36 +0100

    add query & test passing

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -1,57 +1,86 @@
-import merge from 'lodash.merge'
-const testData = {message: 'hello'}
+import merge from 'lodash.merge';
+const testData = { message: 'hello' };
 
 // These are generic methods used in the generic controllers for all models
 export const controllers = {
   createOne(model, body) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   updateOne(docToUpdate, update) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   deleteOne(docToDelete) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getOne(docToGet) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getAll(model) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   findByParam(model, id) {
-    return Promise.resolve(testData)
-  }
-}
+    return Promise.resolve(testData);
+  },
+};
 
-export const createOne = (model) => (req, res, next) => {
+export const createOne = model => (req, res, next) => {
+  return controllers
+    .createOne(model, req.body)
+    .then(doc => res.status(201).json(doc))
+    .catch(error => next(error));
+};
 
-}
+export const updateOne = model => async (req, res, next) => {
+  const docToUpdate = req.docFromId;
+  const update = req.body;
 
-export const updateOne = (model) => async (req, res, next) => {
+  return controllers
+    .updateOne(docToUpdate, update)
+    .then(doc => res.status(201).json(doc))
+    .catch(error => next(error));
+};
 
-}
+export const deleteOne = model => (req, res, next) => {
+  return controllers
+    .deleteOne(req.docFromId)
+    .then(doc => res.status(201).json(doc))
+    .catch(error => next(error));
+};
 
-export const deleteOne = (model) => (req, res, next) => {
+export const getOne = model => (req, res, next) => {
+  return controllers
+    .getOne(req.docToUpdate)
+    .then(doc => res.status(200).json(doc))
+    .catch(error => next(error));
+};
 
-}
+export const getAll = model => (req, res, next) => {
+  return controllers
+    .getAll(model)
+    .then(docs => res.json(docs))
+    .catch(error => next(error));
+};
 
-export const getOne = (model) => (req, res, next) => {
-
-}
-
-export const getAll = (model) => (req, res, next) => {
-
-}
-
-export const findByParam = (model) => (req, res, next, id) => {
-  
-}
-
+export const findByParam = model => (req, res, next, id) => {
+  return controllers
+    .findByParam(model, id)
+    .then(doc => {
+      if (!doc) {
+        next(new Error('Not Found Error'));
+      } else {
+        req.docFromId;
+        next();
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+};
 
 export const generateControllers = (model, overrides = {}) => {
   const defaults = {
@@ -60,8 +89,8 @@ export const generateControllers = (model, overrides = {}) => {
     getOne: getOne(model),
     deleteOne: deleteOne(model),
     updateOne: updateOne(model),
-    createOne: createOne(model)
-  }
+    createOne: createOne(model),
+  };
 
-  return {...defaults, ...overrides}
-}
+  return { ...defaults, ...overrides };
+};
