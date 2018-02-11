a43968f928c5668115f3a96264242e9f549567be
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-02-11 09:15:09 +0100

    add dynamic config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import http from 'http'
+import http from 'http';
+import config from './config';
 // import { execute, subscribe } from 'graphql'
-import { createServer } from 'http'
+import { createServer } from 'http';
 
-import app from './server'
+import app from './server';
 // import schema from './schema'
 
-const server = http.createServer(app)
-let currentApp = app
+const server = http.createServer(app);
+let currentApp = app;
 
-server.listen(3000, () => {
-	console.log('Server listening on port 3000')
-})
+server.listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`);
+});
 
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(['./server'], () => {
+    server.removeListener('request', currentApp);
+    server.on('request', app);
+    currentApp = app;
+  });
 }
