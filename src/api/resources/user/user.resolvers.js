d9a955f8d0fbbe10a3a426d6108515e781657113
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-02-12 11:05:06 +0100

    resolve error cannot return null for non-nullable field User => JSON web Token needed

--- src/api/resources/user/user.resolvers.js
+++ src/api/resources/user/user.resolvers.js
@@ -2,7 +2,12 @@ import { User } from './user.model';
 import merge from 'lodash.merge';
 
 const getMe = (_, __, { user }) => {
-  return user;
+  return {
+    id: 1432453,
+    username: 'hello',
+    createdAt: '12 feb 10h',
+    updatedAt: '12 feb 11h03',
+  };
 };
 
 export const userResolvers = {
