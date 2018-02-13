98681b0eee4088922ccfbede22205a6b7037deb9
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-02-13 10:11:02 +0100

    add song.graphQLRouter

--- src/api/graphQLRouter.js
+++ src/api/graphQLRouter.js
@@ -1,5 +1,6 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import { userType, userResolvers } from './resources/user';
+import { songType } from './resources/song';
 import merge from 'lodash.merge';
 import { graphqlExpress } from 'apollo-server-express';
 
@@ -12,7 +13,7 @@ const baseSchema = `
 
 const schema = makeExecutableSchema({
   // all the graphql files
-  typeDefs: [baseSchema, userType],
+  typeDefs: [baseSchema, userType, songType],
   // all the resolvers
   resolvers: merge({}, userResolvers),
 });
